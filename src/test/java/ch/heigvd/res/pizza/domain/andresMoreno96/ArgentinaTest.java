9dfbe33d35cc8325e45ec1425bce3f242b15b946
andy <andres.moreno@heig-vd.ch>
2019-03-04 10:39:55 +0100

    argentina pizza

--- src/test/java/ch/heigvd/res/pizza/domain/andresMoreno96/ArgentinaTest.java
+++ src/test/java/ch/heigvd/res/pizza/domain/andresMoreno96/ArgentinaTest.java
@@ -1,6 +1,5 @@
 package ch.heigvd.res.pizza.domain.andresMoreno96;
 
-import ch.heigvd.res.pizza.domain.andresMoreno96.Argentina;
 import ch.heigvd.res.pizza.protocol.GreetingsRequest;
 import ch.heigvd.res.pizza.protocol.GreetingsResponse;
 import org.junit.jupiter.api.Test;
@@ -10,8 +9,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class ArgentinaTest {
 
   @Test
-  void ANDYShouldReplyToGreetings() {
-    ANDY luigi = new ANDY();
+  void ArgentinaShouldReplyToGreetings() {
+    Argentina luigi = new Argentina();
     GreetingsRequest request = new GreetingsRequest("Hi there");
     GreetingsResponse response = luigi.greet(request);
     assertEquals("hello, how can I help you?", response.getText());
