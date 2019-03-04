7093657148b2fa331f50c1dc9046705011b73cdf
RengokuryuuhonokaCrimsonFlame <julien.rod@heig-vd.ch>
2019-03-04 10:34:45 +0100

    I add pizza 4 fromages

---
+++ src/main/java/ch/heigvd/res/pizza/domain/RengokuryuuHonokaCrimsonFlame/4Fromages.java
@@ -0,0 +1,21 @@
+package ch.heigvd.res.pizza.domain.wasadigi;
+
+import ch.heigvd.res.pizza.domain.IProduct;
+
+import java.math.BigDecimal;
+
+public class Margherita implements IProduct {
+
+  public final static String NAME = "4 Fromages";
+  public final static BigDecimal PRICE = new BigDecimal(18.0);
+
+  @Override
+  public String getName() {
+    return NAME;
+  }
+
+  @Override
+  public BigDecimal getPrice() {
+    return PRICE;
+  }
+}
---
+++ src/test/java/ch/heigvd/res/pizza/domain/RengokuryuuHonokaCrimsonFlame/4FromagesTest.java
@@ -0,0 +1,32 @@
+package ch.heigvd.res.pizza.domain.wasadigi;
+
+import ch.heigvd.res.pizza.domain.Pizzaiolo;
+import ch.heigvd.res.pizza.protocol.OrderRequest;
+import ch.heigvd.res.pizza.protocol.OrderResponse;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MargheritaTest {
+
+  @Test
+  void thePriceAndNameFor4FromagesShouldBeCorrect() {
+    4Fromages beer = new 4Fromages();
+    assertEquals(beer.getName(), 4Fromages.NAME);
+    assertEquals(beer.getPrice(), 4Fromages.PRICE);
+  }
+
+  @Test
+  void aPizzaioloShouldAcceptAnOrderForMargherita() {
+    Pizzaiolo mario = new Pizzaiolo();
+    String productName = "ch.heigvd.res.pizza.domain.wasadigi.4Fromages";
+    int numberOfPizzas = 2;
+    OrderRequest request = new OrderRequest(numberOfPizzas, productName);
+    OrderResponse response = mario.order(request);
+    BigDecimal expectedTotalPrice = 4Fromages.PRICE.multiply(new BigDecimal(numberOfPizzas));
+    assertEquals(expectedTotalPrice, response.getTotalPrice());
+  }
+
+}
